5471dfddaada7dddb68acabf60118d482e12d09e
Isaac Westaway <i.westaway3119@gmail.com>
2023-06-04 15:58:50 +1000

    typo

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -12,7 +12,7 @@ const Hero: React.FC<Hero> = ({
     action
 }) => {
   return (
-    <div className='flex items-center justify-center h-screen mb-12 bg-fixed bg-center bg-cover custom-img z-50 sm:w-full w-10/12'>
+    <div className='flex items-center justify-center h-screen mb-12 bg-fixed bg-center bg-cover custom-img z-50 sm:w-full w-1/2'>
       {/* Overlay */}
       <div className='absolute top-0 left-0 right-0 bottom-0 bg-black/70 z-[2]' />
       <div className='p-5 text-[#FFFFF7] z-[2] mt-[-10rem]'>
