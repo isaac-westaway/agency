92b4f728e08c2957783627cd1ebb1b878db4eee5
Isaac Westaway <i.westaway3119@gmail.com>
2023-06-10 23:39:23 +1000

    testing purposes

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,6 +23,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
+          <ClientOnly>
+            <Navbar />
+          </ClientOnly>
           <div className="">        
             {children}
             <Analytics />
--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,9 @@
-import Hero from "./components/hero/Hero"
-import Navbar from "./components/navbar/Navbar"
-
-
-
 export default function Home() {
   return (
-    <div>
-      <Navbar/>
-      <div className='absolute top-0 left-0 right-0 bottom-0 bg-black/70 z-[2]'>
-        <div className='p-5 text-[#FFFFF7] z-[2] mt-[-10rem]'>
-          <h2 className='text-5xl font-bold'>test</h2>
-          <p className='py-5 text-xl'>test</p>
-        </div>
+    
+    <div className="flex h-screen w-full items-center bg-orange-50 border-8 border-green-700 p-4">
+      <div className="text-purple-800">
+        <h1 className="text-4xl">Hi Guys</h1>
       </div>
     </div>
   )
