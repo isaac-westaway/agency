7115dd22862242f84604cdb56a166677c701cdc2
Isaac Westaway <i.westaway3119@gmail.com>
2023-06-10 23:20:57 +1000

    testing

--- app/components/hero/Hero.tsx
+++ app/components/hero/Hero.tsx
@@ -17,7 +17,7 @@ const Hero: React.FC<Hero> = ({
     '>
       {/* Overlay */}
       <div className='absolute top-0 left-0 right-0 bottom-0 bg-black/70 z-[2]' />
-      <div className='p-5 text-[#FFFFF7] z-[2] mt-[-10rem]'>
+      <div className='p-5 text-[#FFFFF7] z-[2] mt-[-10rem] userSelect'>
         <h2 className='text-5xl font-bold'>{heading}</h2>
         <p className='py-5 text-xl'>{message}</p>
         <button className='px-8 py-2 border'>{action}</button>
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
 import Hero from "./components/hero/Hero"
+import Navbar from "./components/navbar/Navbar"
+
+
 
 export default function Home() {
   return (
     <div>
+      <Navbar/>
       <Hero heading="Brisbane's go-to in the digital world" 
             message="We design, develop & deploy websites for small businesses throughout Australia" 
             action="Let's talk!"
