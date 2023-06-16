312b282c1153c944886b1306f1e3f2131f0cd74c
Isaac Westaway <i.westaway3119@gmail.com>
2023-06-16 23:01:54 +1000

    fix

--- app/components/testimonials/Testimonials.tsx
+++ app/components/testimonials/Testimonials.tsx
@@ -21,14 +21,33 @@ const Testimonials = () => {
     );
   };
 
-  const swipeHandlers = useSwipeable({
-    onSwipedLeft: () => goForward(),
-    onSwipedRight: () => goBackward(),
-  });
+    const swipeHandlers = useSwipeable({
+      onSwipedLeft: () => goForward(),
+      onSwipedRight: () => goBackward(),
+    });
 
-  useEffect(() => {
-    slideNumberRef.current = slideNumber;
-  }, [slideNumber]);
+    useEffect(() => {
+      slideNumberRef.current = slideNumber;
+    }, [slideNumber]);
+
+    const shimmer = (w: number, h: number) => `
+  <svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+    <defs>
+      <linearGradient id="g">
+        <stop stop-color="#333" offset="20%" />
+        <stop stop-color="#222" offset="50%" />
+        <stop stop-color="#333" offset="70%" />
+      </linearGradient>
+    </defs>
+    <rect width="${w}" height="${h}" fill="#000000" />
+    <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
+    <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
+  </svg>`
+
+const toBase64 = (str: string) =>
+  typeof window === 'undefined'
+    ? Buffer.from(str).toString('base64')
+    : window.btoa(str)
 
     return (
       <div className="relative w-95 mx-auto overflow-hidden" {...swipeHandlers}>
@@ -50,6 +69,7 @@ const Testimonials = () => {
                   alt=""
                   placeholder="blur"
                   loading="lazy"
+                  blurDataURL={`data:image/svg+xml;base64,${toBase64(shimmer(700, 475))}`}
                 />
                 <div className="text-center text-xl sm:text-md font-semibold leading-8 text-white sm:leading-4 h-full">
                 <p className="text-center overflow-clip h-20 flex-1 flex items-center justify-center">{testimonial.quote}</p>
@@ -63,6 +83,7 @@ const Testimonials = () => {
                     alt=""
                     placeholder="blur"
                     loading="lazy"
+                    blurDataURL={`data:image/svg+xml;base64,${toBase64(shimmer(700, 475))}`}
                   />
                   <div className="mt-4 flex items-center justify-center space-x-3 text-base mb-5">
                     <div className="sm:text-sm text-white">{testimonial.name}</div>
