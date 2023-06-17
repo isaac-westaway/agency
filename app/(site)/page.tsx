db9eb9e8ef5ad284f0216deb60f673e124495549
Isaac Westaway <i.westaway3119@gmail.com>
2023-06-17 22:10:58 +1000

    i like it

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -10,20 +10,75 @@ export default function Home() {
         <section id="top" className="snap-start">
           <Hero />
         </section>
-        <section id="body" className="snap-start px-2">
+        <section id="body" className="snap-start px-2 relative">
+          <div className="absolute sm:-top-20 top-36 right-0 sm:-right-0 object-right">
+            <svg
+              width="238"
+              height="531"
+              viewBox="0 0 238 531"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <rect
+                opacity="0.3"
+                x="422.819"
+                y="-70.8145"
+                width="196"
+                height="541.607"
+                rx="2"
+                transform="rotate(51.2997 422.819 -70.8145)"
+                fill="url(#paint0_linear_83:2)"
+              ></rect>
+              <rect
+                opacity="0.3"
+                x="426.568"
+                y="144.886"
+                width="59.7544"
+                height="541.607"
+                rx="2"
+                transform="rotate(51.2997 426.568 144.886)"
+                fill="url(#paint1_linear_83:2)"
+              ></rect>
+              <defs>
+                <linearGradient
+                  id="paint0_linear_83:2"
+                  x1="517.152"
+                  y1="-251.373"
+                  x2="517.152"
+                  y2="459.865"
+                  gradientUnits="userSpaceOnUse"
+                >
+                  <stop stop-color="#4A6CF7"></stop>
+                  <stop offset="1" stop-color="#4A6CF7" stop-opacity="0"></stop>
+                </linearGradient>
+                <linearGradient
+                  id="paint1_linear_83:2"
+                  x1="455.327"
+                  y1="-35.673"
+                  x2="455.327"
+                  y2="675.565"
+                  gradientUnits="userSpaceOnUse"
+                >
+                  <stop stop-color="#4A6CF7"></stop>
+                  <stop offset="1" stop-color="#4A6CF7" stop-opacity="0"></stop>
+                </linearGradient>
+              </defs>
+            </svg>
+          </div>
           <div className="relative mx-auto max-w-7xl px-4 pt-3 sm:px-6 lg:px-8 text-white translate-y-60 xl:translate-y-52 sm:-translate-y-48 grid grid-cols-2 sm:grid-cols-1 grid-row">
-
-            <div className="w-full justify-end pt-5">
-              <span className="text-3xl sm:text-xl font-extrabold sm:leading-tight sm:tracking-tight">
-                View our exceptional range of services
-              </span>
-              <p className="mt-4 max-w-3xl text-lg text-slate-500">
-                Choose an affordable plan that is packed with the best features
-                for enganging your audience, creating customer loyalty and
-                driving sales.
-              </p>
-            </div>
-            <div className="relative flex justify-center sm:justify-start top-0 left-0 w-full">
+            <FramerReveal>
+              <div className="w-full justify-end pt-5">
+                <span className="text-3xl sm:text-xl font-extrabold sm:leading-tight sm:tracking-tight">
+                  View our exceptional range of services
+                </span>
+                <p className="mt-4 max-w-3xl text-lg text-slate-500">
+                  Choose an affordable plan that is packed with the best features
+                  for enganging your audience, creating customer loyalty and
+                  driving sales.
+                </p>
+              </div>
+            </FramerReveal>
+            <div className="relative flex justify-center sm:justify-center sm:pt-10 top-0 left-0 w-full">
               <Image
                 src="/about-image.svg"
                 alt=""
@@ -35,23 +90,19 @@ export default function Home() {
           </div>
         </section>
         <section>
-        <div className="relative mx-auto max-w-7xl px-4 pt-3 sm:px-6 lg:px-8 text-white translate-y-60 sm:-translate-y-48">
-            <div className="w-full justify-end mt-12 border-t-[1px] border-dark-50">
-              <div className="flex justify-center text-center items-center font-extrabold text-3xl pt-9">
-                A full tech and creative team
-              </div>
-              <div className="grid grid-cols-3 sm:grid-cols-1 text-center p-3 gap-2">
-                <div className="text-2xl border">
-                  Hi Guys
+          <div className="relative mx-auto max-w-7xl px-4 pt-3 sm:px-6 lg:px-8 text-white translate-y-60 sm:-translate-y-48 pb-20">
+            <FramerReveal>
+              <div className="w-full justify-end mt-12 border-t-[1px] border-dark-50">
+                <div className="flex justify-center text-center items-center font-extrabold text-3xl pt-9">
+                  A full tech and creative team
                 </div>
-                <div className="text-2xl border">
-                  Hi Guys
-                </div>
-                <div className="text-2xl border">
-                  Hi Guys
+                <div className="grid grid-cols-3 sm:grid-cols-1 text-center p-3 gap-2 h-56">
+                  <div className="relative text-2xl border">Hi Guys</div>
+                  <div className="relative text-2xl border">Hi Guys</div>
+                  <div className="relative text-2xl border">Hi Guys</div>
                 </div>
               </div>
-            </div>
+            </FramerReveal>
           </div>
         </section>
       </div>
--- app/components/hero/Hero.tsx
+++ app/components/hero/Hero.tsx
@@ -269,46 +269,48 @@ const Hero = () => {
       </div>
       <div className="relative z-10 px-6 pt-4 sm:pt-1 sm:-translate-y-20 md:-translate-y-20 lg:-translate-y-20 md:pt-3 lg:pt-6">
         <div className="mx-auto py-44 sm:py-48 lg:pt-56">
-          <div className="text-center">
-            <span className="text-5xl font-bold text-white sm:text-6xl tracking-wider">
-              Design. Develop. Deploy.
-            </span>
-            <div className="mt-6 text-lg leading-8 text-gray-400 font-medium">
-              DigiSpark is Australia&#8217;s top-ranking web services provider.
-              <br />
-              <span className="font-light">
-                We specialize in the full production lifecycle, from designing
-                your site to the deployment of it.
+          <FramerReveal>
+            <div className="text-center">
+              <span className="text-5xl font-bold text-white sm:text-6xl tracking-wider">
+                Design. Develop. Deploy.
               </span>
+              <div className="mt-6 text-lg leading-8 text-gray-400 font-medium">
+                DigiSpark is Australia&#8217;s top-ranking web services provider.
+                <br />
+                <span className="font-light">
+                  We specialize in the full production lifecycle, from designing
+                  your site to the deployment of it.
+                </span>
+              </div>
+              <div className="mt-8 flex items-center justify-center gap-x-6">
+                <a
+                  href="#"
+                  className="rounded-md bg-[#4A6CF7] px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                >
+                  Get started
+                </a>
+                <a
+                  href="#"
+                  className="text-sm font-semibold leading-6 text-white"
+                >
+                  Learn more <span aria-hidden="true">→</span>
+                </a>
+              </div>
             </div>
-            <div className="mt-8 flex items-center justify-center gap-x-6">
-              <a
-                href="#"
-                className="rounded-md bg-[#4A6CF7] px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
-                Get started
-              </a>
-              <a
-                href="#"
-                className="text-sm font-semibold leading-6 text-white"
-              >
-                Learn more <span aria-hidden="true">→</span>
-              </a>
-            </div>
-          </div>
+          </FramerReveal>
         </div>
       </div>
       <div className="relative z-10 mx-auto max-w-7xl px-4 pt-4 sm:px-6 lg:px-8 text-white translate-y-[12rem] lg:-translate-y-2 xl:translate-y-[6.3rem] sm:-translate-y-[12rem]">
-        <FramerReveal>
-          <div className="border-y-[1px] sm:w-95 mx-auto pt-[1.2rem] border-dark-50">
+        <div className="border-y-[1px] sm:w-95 mx-auto pt-[1.2rem] border-dark-50">
+          <FramerReveal>
             <span className="text-3xl font-extrabold sm:text-xl sm:leading-tight sm:tracking-tight">
               See what Australian small businesses are saying about us
             </span>
             <div className="flex justify-center mt-4 text-lg text-gray-500 w-full border rounded-2xl border-dark-50 mb-[2.5rem] relative ">
                 <Testimonials />
             </div>
-          </div>
-        </FramerReveal>
+          </FramerReveal>
+        </div>
       </div>
     </div>
   );
