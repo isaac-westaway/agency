bcf93c7e8a53b0899c119ae151a14d43dd2217af
Isaac Westaway <i.westaway3119@gmail.com>
2023-06-17 21:41:58 +1000

    another test

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -12,68 +12,7 @@ export default function Home() {
         </section>
         <section id="body" className="snap-start px-2">
           <div className="relative mx-auto max-w-7xl px-4 pt-3 sm:px-6 lg:px-8 text-white translate-y-60 xl:translate-y-52 sm:-translate-y-48 grid grid-cols-2 sm:grid-cols-1 grid-row">
-            <div className="absolute -top-11 -right-28 overflow-hidden object-none">
-              <svg
-                width="238"
-                height="531"
-                viewBox="0 0 238 531"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <rect
-                  opacity="0.3"
-                  x="422.819"
-                  y="-70.8145"
-                  width="196"
-                  height="541.607"
-                  rx="2"
-                  transform="rotate(51.2997 422.819 -70.8145)"
-                  fill="url(#paint0_linear_83:2)"
-                ></rect>
-                <rect
-                  opacity="0.3"
-                  x="426.568"
-                  y="144.886"
-                  width="59.7544"
-                  height="541.607"
-                  rx="2"
-                  transform="rotate(51.2997 426.568 144.886)"
-                  fill="url(#paint1_linear_83:2)"
-                ></rect>
-                <defs>
-                  <linearGradient
-                    id="paint0_linear_83:2"
-                    x1="517.152"
-                    y1="-251.373"
-                    x2="517.152"
-                    y2="459.865"
-                    gradientUnits="userSpaceOnUse"
-                  >
-                    <stop stop-color="#4A6CF7"></stop>
-                    <stop
-                      offset="1"
-                      stop-color="#4A6CF7"
-                      stop-opacity="0"
-                    ></stop>
-                  </linearGradient>
-                  <linearGradient
-                    id="paint1_linear_83:2"
-                    x1="455.327"
-                    y1="-35.673"
-                    x2="455.327"
-                    y2="675.565"
-                    gradientUnits="userSpaceOnUse"
-                  >
-                    <stop stop-color="#4A6CF7"></stop>
-                    <stop
-                      offset="1"
-                      stop-color="#4A6CF7"
-                      stop-opacity="0"
-                    ></stop>
-                  </linearGradient>
-                </defs>
-              </svg>
-            </div>
+
             <div className="w-full justify-end pt-5">
               <span className="text-3xl sm:text-xl font-extrabold sm:leading-tight sm:tracking-tight">
                 View our exceptional range of services
