cab94ffd3a8f0dd9558118e5f35859680170df7a
Isaac Westaway <i.westaway3119@gmail.com>
2023-06-17 21:39:58 +1000

    test

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -6,7 +6,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <div className="scroll-smooth	snap-x h-[1920px] overflow-hidden">
+      <div className="scroll-smooth	snap-x">
         <section id="top" className="snap-start">
           <Hero />
         </section>
@@ -97,8 +97,8 @@ export default function Home() {
         </section>
         <section>
         <div className="relative mx-auto max-w-7xl px-4 pt-3 sm:px-6 lg:px-8 text-white translate-y-60 sm:-translate-y-48">
-            <div className="w-full justify-end mt-12 border">
-              <div className="flex justify-center text-center items-center font-extrabold text-3xl">
+            <div className="w-full justify-end mt-12 border-t-[1px] border-dark-50">
+              <div className="flex justify-center text-center items-center font-extrabold text-3xl pt-9">
                 A full tech and creative team
               </div>
               <div className="grid grid-cols-3 sm:grid-cols-1 text-center p-3 gap-2">
