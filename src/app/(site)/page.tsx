8714a077ed14406d6d1e31c4845f7ded38d952d4
Isaac Westaway <i.westaway3119@gmail.com>
2023-06-23 16:29:27 +1000

    final

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -31,156 +31,158 @@ export default function Home() {
       <section id="top" className="snap-start">
         <Hero />
       </section>
-      <section className="relative mt-[11rem] sm:-mt-[8.8rem] md:mt-[0.4rem] lg:mt-[2rem] xl:mt-[5rem] border-t-[1px] border-dark-50 text-white ml-11 mr-11">
-        <div className="absolute top-1/12 -top-12 -right-11 -z-50">
-          <Image src={Timeline} style={{ transform: "scaleX(-1)" }} alt="" />
-        </div>
-        <div className="w-full">
-          <RevealOnce>
-            <div className="flex justify-center text-center items-center font-extrabold text-3xl pt-8 select-text">
-              <span className="sm:w-full md:w-full tracking-wider">
-                A full tech and creative team at your fingertips.
-              </span>
+      <div className="">
+          <section className="relative mt-[11.4rem] sm:-mt-[8.8rem] md:mt-[0.4rem] lg:mt-[2rem] xl:mt-[5rem] border-t-[1px] border-dark-50 text-white ml-11 mr-11">
+            <div className="absolute top-1/12 -top-12 -right-11 -z-50">
+              <Image src={Timeline} style={{ transform: "scaleX(-1)" }} alt="" />
             </div>
-          </RevealOnce>
-          <RevealOnce>
-            <div className="flex justify-center text-center items-center font-normal text-lg py-8 select-text tracking-wider">
-              <span className="text-gray-400">
-                We specialise in UI/UX design, website development and website
-                deployment.
-                <div className="text-lg leading-8 text-gray-400 font-medium">
-                  The full stack.
+            <div className="w-full">
+              <RevealOnce>
+                <div className="flex justify-center text-center items-center font-extrabold text-3xl pt-8 select-text">
+                  <span className="sm:w-full md:w-full tracking-wider">
+                    A full tech and creative team at your fingertips.
+                  </span>
                 </div>
-              </span>
+              </RevealOnce>
+              <RevealOnce>
+                <div className="flex justify-center text-center items-center font-normal text-lg py-8 select-text tracking-wider">
+                  <span className="text-gray-400">
+                    We specialise in UI/UX design, website development and website
+                    deployment.
+                    <div className="text-lg leading-8 text-gray-400 font-medium">
+                      The full stack.
+                    </div>
+                  </span>
+                </div>
+              </RevealOnce>
+              <div className="grid grid-cols-3 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-1 gap-6">
+                <RevealOnce>
+                  <div className="testimonial-container group relative shadow-lg p-8 flex flex-col text-2xl border-[2px] border-dark-50 overflow-hidden rounded-2xl hover:border-[#FBFAF6] hover:text-[#FBFAF6] transition ease-in-out duration-500 h-[26rem] backdrop-blur-sm">
+                    <div className="absolute top-5 right-12">
+                      <Image
+                        src={Design}
+                        alt="Design Symbol"
+                        className="w-10 h-10"
+                      />
+                    </div>
+                    <div className="absolute -mx-8 bottom-0 w-full"></div>
+                    <h3 className="text-2xl font-semibold leading-5 z-50">
+                      Design
+                    </h3>
+                    <ul className="mt-4 space-y-4 flex-1 z-50">
+                      <li className="flex text-lg leading-8  xl:text-base">
+                        <span className="text-start">
+                          <span className="flex border-b-[1px] border-dark-50 w-full p-2 mb-1 justify-center">
+                            Professional UI/UX Design
+                          </span>
+                          <div className="mt-2 text-gray-200 text-opacity-95 text-base">
+                            UI/UX is the User Interface (UI) and User Experience
+                            (UX) of an app or a website.
+                          </div>
+                        </span>
+                      </li>
+                    </ul>
+                    <div className="mt-4 items-center text-center z-50">
+                      <span className="text-[16px] border-b-[2px] pb-[1px] z-50 font-extrabold text-white group-hover:text-orange-600 group-hover:border-b-orange-600 hover:cursor-pointer transition duration-200 ease-in-out">
+                        <a
+                          className="hover:text-orange-400 hover:border-b-orange-400 hover:border-b-[2px] scroll-pt-96 snap-start"
+                          href="#design"
+                        >
+                          READ MORE
+                        </a>
+                      </span>
+                    </div>
+                  </div>
+                </RevealOnce>
+                <RevealOnce>
+                  <div className="testimonial-container group shadow-lg p-8 overflow-hidden flex flex-col relative text-2xl border-[2px] border-dark-50 hover:border-[#FBFAF6] hover:text-[#FBFAF6] rounded-2xl transition ease-in-out duration-500 h-[26rem] backdrop-blur-sm">
+                    <div className="absolute top-[1.2rem] right-11">
+                      <Image
+                        src={Develop}
+                        alt="Develop Symbol"
+                        className="w-12 h-12"
+                      />
+                    </div>
+                    <div className="absolute -mx-8 bottom-0 w-full"></div>
+                    <h3 className="text-2xl font-semibold leading-5 z-50">
+                      Develop
+                    </h3>
+                    <ul className="mt-4 space-y-4 flex-1 z-50">
+                      <li className="flex flex-1 text-lg xl:text-base leading-8">
+                        <span className="text-start">
+                          <span className="flex border-b-[1px] border-dark-50 w-full p-2 mb-1 justify-center">
+                            Industry leading web development
+                          </span>
+                          <div className="mt-2 text-gray-200 text-opacity-95">
+                            We develop
+                          </div>
+                        </span>
+                      </li>
+                    </ul>
+                    <div className="mt-4 items-center text-center z-50">
+                      <span className="text-[16px] border-b-[2px] pb-[1px] z-50 font-extrabold text-white group-hover:text-orange-600 group-hover:border-b-orange-600 hover:cursor-pointer transition duration-200 ease-in-out">
+                        <a
+                          className="hover:text-orange-400 hover:border-b-orange-400 hover:border-b-[2px]"
+                          href="#develop"
+                        >
+                          READ MORE
+                        </a>
+                      </span>
+                    </div>
+                  </div>
+                </RevealOnce>
+                <RevealOnce>
+                  <div className="testimonial-container group shadow-lg p-8 flex flex-col relative text-2xl border-[2px] border-dark-50 hover:border-[#FBFAF6] hover:text-[#FBFAF6] rounded-2xl transition ease-in-out duration-500 h-[26rem] backdrop-blur-sm">
+                    <div className="absolute top-[1.3rem] right-11">
+                      <Image
+                        src="deploy.svg"
+                        alt="Deploy Symbol"
+                        className="hover:fill-white fill"
+                        width="48"
+                        height="48"
+                      />
+                    </div>
+                    <h3 className="text-2xl font-semibold leading-5 z-50">
+                      Deploy
+                    </h3>
+                    <ul className="mt-4 flex-1 z-50">
+                      <li className="flex flex-col text-lg leading-8  xl:text-base">
+                        <span className="text-start">
+                          <span className="flex border-b-[1px] border-dark-50 w-full p-2 mb-1 justify-center">
+                            Streamlined digital deployment
+                          </span>
+                          <div className="mt-2 text-gray-200 text-opacity-95">
+                            We&#39;ll handle the seamless deployment of your
+                            website, ensuring it&#39;s optimized for performance,
+                            security, and accessibility across different platforms
+                            and devices.
+                          </div>
+                        </span>
+                      </li>
+                    </ul>
+                    <div className="mt-4 items-center text-center z-50">
+                      <span className="text-[16px] border-b-[2px] pb-[1px] z-50 font-extrabold text-white group-hover:text-orange-600 group-hover:border-b-orange-600 hover:cursor-pointer transition duration-200 ease-in-out">
+                        <a
+                          className="hover:text-orange-400 hover:border-b-orange-400 hover:border-b-[2px]"
+                          href="#deploy"
+                        >
+                          READ MORE
+                        </a>
+                      </span>
+                    </div>
+                  </div>
+                </RevealOnce>
+              </div>
             </div>
-          </RevealOnce>
-          <div className="grid grid-cols-3 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-1 gap-6">
-            <RevealOnce>
-              <div className="testimonial-container group relative shadow-lg p-8 flex flex-col text-2xl border-[2px] border-dark-50 overflow-hidden rounded-2xl hover:border-[#FBFAF6] hover:text-[#FBFAF6] transition ease-in-out duration-500 h-[26rem] backdrop-blur-sm">
-                <div className="absolute top-5 right-12">
-                  <Image
-                    src={Design}
-                    alt="Design Symbol"
-                    className="w-10 h-10"
-                  />
-                </div>
-                <div className="absolute -mx-8 bottom-0 w-full"></div>
-                <h3 className="text-2xl font-semibold leading-5 z-50">
-                  Design
-                </h3>
-                <ul className="mt-4 space-y-4 flex-1 z-50">
-                  <li className="flex text-lg leading-8  xl:text-base">
-                    <span className="text-start">
-                      <span className="flex border-b-[1px] border-dark-50 w-full p-2 mb-1 justify-center">
-                        Professional UI/UX Design
-                      </span>
-                      <div className="mt-2 text-gray-200 text-opacity-95 text-base">
-                        UI/UX is the User Interface (UI) and User Experience
-                        (UX) of an app or a website.
-                      </div>
-                    </span>
-                  </li>
-                </ul>
-                <div className="mt-4 items-center text-center z-50">
-                  <span className="text-[16px] border-b-[2px] pb-[1px] z-50 font-extrabold text-white group-hover:text-orange-600 group-hover:border-b-orange-600 hover:cursor-pointer transition duration-200 ease-in-out">
-                    <a
-                      className="hover:text-orange-400 hover:border-b-orange-400 hover:border-b-[2px] scroll-pt-96 snap-start"
-                      href="#design"
-                    >
-                      READ MORE
-                    </a>
-                  </span>
-                </div>
+            <RevealAlways>
+              <div className="flex items-center justify-center py-8">
+                <button className="text-center sm:text-sm px-6 py-3 rounded-2xl bg-[#4A6CF7] hover:bg-white hover:text-black transition duration-200 ease-in-out">
+                  Start building your online presence
+                </button>
               </div>
-            </RevealOnce>
-            <RevealOnce>
-              <div className="testimonial-container group shadow-lg p-8 overflow-hidden flex flex-col relative text-2xl border-[2px] border-dark-50 hover:border-[#FBFAF6] hover:text-[#FBFAF6] rounded-2xl transition ease-in-out duration-500 h-[26rem] backdrop-blur-sm">
-                <div className="absolute top-[1.2rem] right-11">
-                  <Image
-                    src={Develop}
-                    alt="Develop Symbol"
-                    className="w-12 h-12"
-                  />
-                </div>
-                <div className="absolute -mx-8 bottom-0 w-full"></div>
-                <h3 className="text-2xl font-semibold leading-5 z-50">
-                  Develop
-                </h3>
-                <ul className="mt-4 space-y-4 flex-1 z-50">
-                  <li className="flex flex-1 text-lg xl:text-base leading-8">
-                    <span className="text-start">
-                      <span className="flex border-b-[1px] border-dark-50 w-full p-2 mb-1 justify-center">
-                        Industry leading web development
-                      </span>
-                      <div className="mt-2 text-gray-200 text-opacity-95">
-                        We develop
-                      </div>
-                    </span>
-                  </li>
-                </ul>
-                <div className="mt-4 items-center text-center z-50">
-                  <span className="text-[16px] border-b-[2px] pb-[1px] z-50 font-extrabold text-white group-hover:text-orange-600 group-hover:border-b-orange-600 hover:cursor-pointer transition duration-200 ease-in-out">
-                    <a
-                      className="hover:text-orange-400 hover:border-b-orange-400 hover:border-b-[2px]"
-                      href="#develop"
-                    >
-                      READ MORE
-                    </a>
-                  </span>
-                </div>
-              </div>
-            </RevealOnce>
-            <RevealOnce>
-              <div className="testimonial-container group shadow-lg p-8 flex flex-col relative text-2xl border-[2px] border-dark-50 hover:border-[#FBFAF6] hover:text-[#FBFAF6] rounded-2xl transition ease-in-out duration-500 h-[26rem] backdrop-blur-sm">
-                <div className="absolute top-[1.3rem] right-11">
-                  <Image
-                    src="deploy.svg"
-                    alt="Deploy Symbol"
-                    className="hover:fill-white fill"
-                    width="48"
-                    height="48"
-                  />
-                </div>
-                <h3 className="text-2xl font-semibold leading-5 z-50">
-                  Deploy
-                </h3>
-                <ul className="mt-4 flex-1 z-50">
-                  <li className="flex flex-col text-lg leading-8  xl:text-base">
-                    <span className="text-start">
-                      <span className="flex border-b-[1px] border-dark-50 w-full p-2 mb-1 justify-center">
-                        Streamlined digital deployment
-                      </span>
-                      <div className="mt-2 text-gray-200 text-opacity-95">
-                        We&#39;ll handle the seamless deployment of your
-                        website, ensuring it&#39;s optimized for performance,
-                        security, and accessibility across different platforms
-                        and devices.
-                      </div>
-                    </span>
-                  </li>
-                </ul>
-                <div className="mt-4 items-center text-center z-50">
-                  <span className="text-[16px] border-b-[2px] pb-[1px] z-50 font-extrabold text-white group-hover:text-orange-600 group-hover:border-b-orange-600 hover:cursor-pointer transition duration-200 ease-in-out">
-                    <a
-                      className="hover:text-orange-400 hover:border-b-orange-400 hover:border-b-[2px]"
-                      href="#deploy"
-                    >
-                      READ MORE
-                    </a>
-                  </span>
-                </div>
-              </div>
-            </RevealOnce>
-          </div>
-        </div>
-        <RevealAlways>
-          <div className="flex items-center justify-center py-8">
-            <button className="text-center sm:text-sm px-6 py-3 rounded-2xl bg-[#4A6CF7] hover:bg-white hover:text-black transition duration-200 ease-in-out">
-              Start building your online presence
-            </button>
-          </div>
-        </RevealAlways>
-      </section>
+            </RevealAlways>
+          </section>
+      </div>
       <section className="relative text-white overflow-x-hidden ml-11 mr-11 border-t-[1px] border-dark-50">
         <div className="relative overflow-y-hidden py-8 px-4">
           <div className="grid grid-cols-2 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-1 gap-6 sm:gap-0">
