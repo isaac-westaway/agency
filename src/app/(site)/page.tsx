0ea2b785a2799a7639c13e14dcfceadd4c8f7541
Isaac Westaway <i.westaway3119@gmail.com>
2023-06-24 14:46:39 +1000

    e

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -20,7 +20,7 @@ import Testimonials from "../components/testimonials/Testimonials";
 
 export default function Home() {
   return (
-    <div className="scroll-smooth snap-x overscroll-x-none min-h-screen ml-11 mr-11 sm:ml-6 sm:mr-6">
+    <div className="scroll-smooth snap-x overscroll-x-none min-h-screen ml-11 mr-11 sm:ml-6 sm:mr-6 ">
       <Head>
         <title>Digispark Home</title>
         <meta
@@ -33,7 +33,7 @@ export default function Home() {
       </section>
       <div className="">
         <section className="relative mt-[11.4rem] sm:-mt-[13.7rem] md:mt-[0.4rem] lg:mt-[2rem] xl:mt-[5rem] border-t-[1px] border-dark-50 text-white">
-          <div className="absolute top-1/12 -top-12 -right-11 -z-50">
+          <div className="absolute top-1/12 -top-12 -right-11 sm:-right-6 -z-50 overflow-x-hidden">
             <Image src={Timeline} style={{ transform: "scaleX(-1)" }} alt="" />
           </div>
           <div className="w-full">
@@ -98,7 +98,7 @@ export default function Home() {
                 <div className="testimonial-container group relative shadow-lg p-8 flex flex-col text-2xl border-[2px] border-dark-50 overflow-hidden rounded-2xl hover:border-[#FBFAF6] hover:text-[#FBFAF6] transition ease-in-out duration-500 h-[26rem] backdrop-blur-sm">
                   <div className="absolute top-5 right-12">
                     <Image
-                      src={Design}
+                      src={Develop}
                       alt="Design Symbol"
                       className="w-10 h-10"
                     />
@@ -186,12 +186,12 @@ export default function Home() {
       </div>
       <section className="relative text-white overflow-x-hidden border-t-[1px] border-dark-50">
         <div className="relative overflow-y-hidden">
-          <div className="grid grid-cols-2 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-1 gap-6 sm:gap-0 px-4">
+          <div className="grid grid-cols-2 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-1 gap-6 sm:gap-0 md:gap-0 lg:gap-0 px-4">
             <div className="h-full relative">
-              <div className="mt-4 py-4 text-3xl sm:text-lg font-semibold border-y-[1px] border-dark-50">
+              <div className="mt-8 py-4 text-3xl sm:text-lg font-semibold border-y-[1px] border-dark-50">
                 We are committed to delivering the highest quality of work
               </div>
-              <div className="text-base font-light py-4 leading-relaxed">
+              <div className="text-base font-light pt-4 leading-relaxed">
                 Transforming Brisbane&#39;s digital landscape since 2012, DigiSpark
                 is your go-to online service agency. Our team of experts in web
                 design, development, digital strategy, and marketing create
@@ -200,7 +200,7 @@ export default function Home() {
                 Whether you&#39;re a small business or a large enterprise, let&#39;s
                 connect and embark on a digital journey together.
               </div>
-              <div className="flex justify-center w-full pt-16 sm:pt-8 pb-20 items-end">
+              <div className="flex justify-center w-full items-end pt-4 mb-4">
                 <RevealAlways>
                   <div
                     className="rounded-lg px-3.5 py-2.5 cursor-pointer text-white bg-[#4A6CF7] hover:bg-white hover:text-black transition ease-in-out duration-200"
@@ -210,7 +210,88 @@ export default function Home() {
                 </RevealAlways>
               </div>
             </div>
-            <div className="flex items-center justify-center overflow-x-hidden w-full px-4 bg-dark-50 bg-opacity-10 rounded-2xl my-4">
+            <div className="flex mt-8 mb-8 sm:mt-0 md:mt-0 lg:mt-0 items-center justify-center overflow-x-hidden w-full px-4 bg-dark-50 bg-opacity-10 rounded-2xl">
+              <FlyIn flyInRight={true}>
+                <Image
+                  src="/design.webp"
+                  alt="UI/UX"
+                  width="600"
+                  height="800"
+                  className="z-[1000] object-contain mx-auto"
+                />
+              </FlyIn>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="relative text-white overflow-x-hidden border-t-[1px] border-dark-50">
+        <div className="relative overflow-y-hidden">
+          <div className="grid grid-cols-2 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-1 gap-6 sm:gap-0 md:gap-0 lg:gap-0 px-4">
+          <div className="flex my-8 items-center justify-center overflow-x-hidden w-full px-4 bg-dark-50 bg-opacity-10 rounded-2xl">
+              <FlyIn flyInRight={false}>
+                <Image
+                  src="/design.webp"
+                  alt="UI/UX"
+                  width="600"
+                  height="800"
+                  className="z-[1000] object-contain mx-auto"
+                />
+              </FlyIn>
+            </div>
+            <div className="h-full relative">
+              <div className="sm:mt-0 md:mt-0 lg:mt-0 mt-8 py-4 text-3xl sm:text-lg font-semibold border-y-[1px] border-dark-50">
+                We are committed to delivering the highest quality of work
+              </div>
+              <div className="text-base font-light pt-4 leading-relaxed">
+                Transforming Brisbane&#39;s digital landscape since 2012, DigiSpark
+                is your go-to online service agency. Our team of experts in web
+                design, development, digital strategy, and marketing create
+                tailored solutions for your business. From visually stunning
+                websites to ROI-driven marketing campaigns, we deliver results.
+                Whether you&#39;re a small business or a large enterprise, let&#39;s
+                connect and embark on a digital journey together.
+              </div>
+              <div className="flex justify-center w-full items-end pt-4 pb-5">
+                <RevealAlways>
+                  <div
+                    className="rounded-lg px-3.5 py-2.5 cursor-pointer text-white bg-[#4A6CF7] hover:bg-white hover:text-black transition ease-in-out duration-200"
+                  >
+                    I&#39;m Interested!
+                  </div>
+                </RevealAlways>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </section>
+      <section className="relative text-white overflow-x-hidden border-t-[1px] border-dark-50">
+        <div className="relative overflow-y-hidden">
+          <div className="grid grid-cols-2 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-1 gap-6 sm:gap-0 md:gap-0 lg:gap-0 px-4">
+            <div className="h-full relative">
+              <div className="mt-8 py-4 text-3xl sm:text-lg font-semibold border-y-[1px] border-dark-50">
+                We are committed to delivering the highest quality of work
+              </div>
+              <div className="text-base font-light pt-4 leading-relaxed">
+                Transforming Brisbane&#39;s digital landscape since 2012, DigiSpark
+                is your go-to online service agency. Our team of experts in web
+                design, development, digital strategy, and marketing create
+                tailored solutions for your business. From visually stunning
+                websites to ROI-driven marketing campaigns, we deliver results.
+                Whether you&#39;re a small business or a large enterprise, let&#39;s
+                connect and embark on a digital journey together.
+              </div>
+              <div className="flex justify-center w-full items-end pt-4 mb-4">
+                <RevealAlways>
+                  <div
+                    className="rounded-lg px-3.5 py-2.5 cursor-pointer text-white bg-[#4A6CF7] hover:bg-white hover:text-black transition ease-in-out duration-200"
+                  >
+                    I&#39;m Interested!
+                  </div>
+                </RevealAlways>
+              </div>
+            </div>
+            <div className="flex mt-8 mb-8 sm:mt-0 md:mt-0 lg:mt-0 items-center justify-center overflow-x-hidden w-full px-4 bg-dark-50 bg-opacity-10 rounded-2xl">
               <FlyIn flyInRight={true}>
                 <Image
                   src="/design.webp"
