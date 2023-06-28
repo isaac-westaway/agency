b5536b575cf84aaaefa7ad4bc35bb617c98ae2a2
Isaac Westaway <i.westaway3119@gmail.com>
2023-06-28 21:14:40 +1000

    message

--- src/app/components/Accordion.tsx
+++ src/app/components/Accordion.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import React, { useState } from "react";
+import RevealOnce from "./utils/RevealOnce";
 
 interface AccordionItem {
   title: string;
@@ -29,47 +30,49 @@ const Accordion: React.FC<AccordionProps> = ({ items }) => {
     const isActive = activeIndices.includes(index);
 
     return (
-      <div
-        key={index}
-        className={`border border-gray-300 rounded-2xl p-2 cursor-pointer ${
-          isActive ? "bg-black" : ""
-        }`}
-      >
+      <RevealOnce key={index}>
         <div
-          className="flex justify-between items-center"
-          onClick={() => handleClick(index)}
-        >
-          <h3
-            className={`text-lg sm:text-md font-medium py-4 px-4 ${
-              isActive ? "text-[#4463e2]" : "text-white"
-            }`}
-          >
-            {item.title}
-          </h3>
-          <svg
-            className={`w-4 h-4 shrink-0 transition-transform transform ${
-              isActive ? "rotate-180" : ""
-            }`}
-            viewBox="0 0 24 24"
-          >
-            <path fill="currentColor" d="M19 9l-7 7-7-7" />
-          </svg>
-        </div>
-        <div
-          className={`overflow-hidden transition-all duration-[1200ms] ease-in-out ${
-            isActive ? "max-h-[1000px]" : "max-h-0"
+          key={index}
+          className={`border border-gray-300 rounded-2xl p-2 cursor-pointer ${
+            isActive ? "bg-black" : ""
           }`}
-          style={{ pointerEvents: isActive ? "auto" : "none" }}
         >
+            <div
+              className="flex justify-between items-center"
+              onClick={() => handleClick(index)}
+            >
+              <h3
+                className={`text-lg sm:text-md font-medium py-4 px-4 ${
+                  isActive ? "text-[#4463e2]" : "text-white"
+                }`}
+              >
+                {item.title}
+              </h3>
+              <svg
+                className={`w-4 h-4 shrink-0 transition-transform transform ${
+                  isActive ? "rotate-180" : ""
+                }`}
+                viewBox="0 0 24 24"
+              >
+                <path fill="currentColor" d="M19 9l-7 7-7-7" />
+              </svg>
+            </div>
           <div
-            className={`mx-4 py-4 border-t-[1px] text-white cursor-text ${
-              item.contentClassName || ""
+            className={`overflow-hidden transition-all duration-[1200ms] ease-in-out ${
+              isActive ? "max-h-[1000px]" : "max-h-0"
             }`}
+            style={{ pointerEvents: isActive ? "auto" : "none" }}
           >
-            {item.content}
+            <div
+              className={`mx-4 py-4 border-t-[1px] text-white cursor-text ${
+                item.contentClassName || ""
+              }`}
+            >
+              {item.content}
+            </div>
           </div>
         </div>
-      </div>
+      </RevealOnce>
     );
   });
 
