5659fee88c1792ee844e336894698927eb9b7a8b
Isaac Westaway <i.westaway3119@gmail.com>
2023-07-05 13:33:35 +1000

    More SEO

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: process.env.SITE_URL || "https://sparkdigital.vercel.app/",
+    siteUrl: process.env.SITE_URL || "https://wyndigital.net/",
     generateRobotsTxt: true, // (optional)
   // ...other options
 };
