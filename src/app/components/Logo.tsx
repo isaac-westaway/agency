0fd64a1d05a62fec0e49993f2f4118809da65aaa
Isaac Westaway <i.westaway3119@gmail.com>
2023-07-08 11:23:06 +1000

    PNG

--- src/app/components/Logo.tsx
+++ src/app/components/Logo.tsx
@@ -5,10 +5,10 @@ const Logo = () => {
   const router = useRouter();
   return (
     <Image
-      alt="WynDigital Logo SVG Image"
+      alt="WynDigital Logo Image"
       width={150}
       height={150}
-      src="/logo/fulllogo.svg"
+      src="/logo/fulllogo.png"
       loading="eager"
       priority={true}
       className="w-[150px] sm:w-[120px] md:w-[120px] lg:w-[130px] p-0 md:block cursor-pointer shrink-0"
